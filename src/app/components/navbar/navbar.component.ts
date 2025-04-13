e3d719dc6ab0ad00a69d27c7395b30fe73675ff3
rohithreddydepa <rohithreddydepa@gmail.com>
2025-04-13 17:52:17 -0500

    dynamic color change is added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,4 +7,21 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  currentColor = '#4ade80';
+
+  updatePrimaryColor(event: Event) {
+    const newColor = (event.target as HTMLInputElement).value;
+    this.currentColor = newColor;
+    document.documentElement.style.setProperty('--primary-color', newColor);
+    localStorage.setItem('primaryColor', newColor);
+  }
+
+  ngOnInit() {
+    const savedColor = localStorage.getItem('primaryColor');
+    if (savedColor) {
+      this.currentColor = savedColor;
+      document.documentElement.style.setProperty('--primary-color', savedColor);
+    }
+  }
+}
